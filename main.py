c4c1280a4313d1f23172a0b8e242f7c9fffafc4f
R9_ <81550154+Razboinicul@users.noreply.github.com>
2023-04-23 16:53:52 +0300

    Project loading and saving

--- main.py
+++ main.py
@@ -1,11 +1,103 @@
 import tkinter as tk
+from shutil import copytree
+from os.path import exists
+from pathlib import Path
 map = []
 file_entry = None
 new_map = {}
-def load():
-    global path_entry, map
-    filepath = path_entry.get()
-    print(filepath)
+path_entry = None
+row = None
+col = None
+obj = None
+project_n = None
+path_entry = None
+mode_choice = None
+project_name = None
+project_path = None
+project_mode = None
+project_file_path = None
+options = {}
+mode_options = ["Singleplayer"]
+#                "Multiplayer"]
+
+def start():
+    root = tk.Tk()
+    root.title("Project")
+    new_button = tk.Button(root, text="New Project", width=20, command=lambda: new_project_ui(root)).grid(row=0, column=0)
+    load_button = tk.Button(root, text="Load Project", width=20, command=lambda: load_project_ui(root)).grid(row=1, column=0)
+    root.mainloop()
+
+def create_project(_root):
+    global project_n, path_entry, mode_choice, project_name, project_path, project_mode
+    project_name = project_n.get()
+    project_path = path_entry.get()
+    project_mode = mode_choice.get()
+    _root.destroy()
+    print(project_name, project_path, project_mode)
+    project_path = project_path.replace('\\', '/')
+    exe_path = Path(__file__).parent.resolve()
+    if project_mode == "Singleplayer":
+        if not exists(project_path):
+            copytree(f"{exe_path}\\sp_example", project_path)
+        else:
+            print("path already exists")
+    with open(f"{project_path}\\project.sdlwolf", "w") as f:
+        write_text = f"""name {project_name} 
+mode {project_mode}"""
+        f.write(write_text)
+        f.close()
+    main()
+    
+def load_project(_root, path):
+    global project_name, project_path, project_mode, options
+    _root.destroy()
+    project_path = path.replace("/", "\\")
+    f = open(f"{project_path}\\project.sdlwolf", "r")
+    lines = f.readlines()
+    print(lines)
+    f.close()
+    i=0
+    for l in lines:
+        options[i] = l.split()
+        i+=1
+    project_name = options[0][1]
+    project_mode = options[1][1]
+    main()
+    
+def new_project_ui(_root):
+    global mode_options, project_n, path_entry, mode_choice
+    _root.destroy()
+    root = tk.Tk()
+    root.title("New Project")
+    np_label = tk.Label(root, text="New Project").grid(row=0, column=0)
+    name_label = tk.Label(root, text="Project Name: ").grid(row=1, column=0)
+    project_n = tk.Entry(root,  width=20)
+    project_n.grid(row=2, column=0)
+    path_label = tk.Label(root, text="Path: ").grid(row=3, column=0)
+    path_entry = tk.Entry(root,  width=20)
+    path_entry.grid(row=4, column=0)
+    gamemode_label = tk.Label(root, text="Gamemode: ").grid(row=5, column=0)
+    mode_choice = tk.StringVar(root)
+    mode_choice.set(mode_options[0]) # default value
+    mode_choice_o = tk.OptionMenu(root, mode_choice, *mode_options)
+    mode_choice_o.grid(row=6, column=0)
+    create_button = tk.Button(root, text="Create Project", width=20, command=lambda: create_project(root)).grid(row=7, column=0)
+    root.mainloop()
+
+def load_project_ui(_root):
+    global mode_options, project_n, path_entry, mode_choice
+    _root.destroy()
+    root = tk.Tk()
+    root.title("New Project")
+    np_label = tk.Label(root, text="Load Project").grid(row=0, column=0)
+    path_label = tk.Label(root, text="Path: ").grid(row=3, column=0)
+    path_entry = tk.Entry(root,  width=20)
+    path_entry.grid(row=4, column=0)
+    create_button = tk.Button(root, text="Load Project", width=20, command=lambda: load_project(root, path_entry.get())).grid(row=7, column=0)
+    root.mainloop()
+    
+def load(filepath):
+    global map
     try:
         raw_map = open(filepath, "r")
         map = raw_map.readlines()
@@ -28,10 +120,7 @@ def set():
     print(_row, _col, _obj)
     new_map[_row][_col] = str(_obj)
     
-def save():
-    global path_entry
-    path = path_entry.get()
-    print(path)
+def save(path):
     text = """"""
     for i in new_map:
         _text = " ".join(new_map[i])
@@ -44,16 +133,18 @@ def save():
     
 def back(root):
     root.destroy()
+    main()
 
 def main():
     root = tk.Tk()
     root.title("Select")
     root.resizable(False, False)
     map_editor_button = tk.Button(root, text='Map Editor', width=30, command=lambda: map_editor(root)).grid(row=0, column=0)
-    exit_button =tk.Button(root, text='Exit', width=30, command=lambda: root.destroy()).grid(row=1, column=0)
+    exit_button = tk.Button(root, text='Exit', width=30, command=lambda: root.destroy()).grid(row=1, column=0)
     root.mainloop()
 
 def map_editor(_root):
+    global col, row, obj, project_path
     _root.destroy()
     root = tk.Tk()
     root.title("Map Editor")
@@ -64,13 +155,11 @@ def map_editor(_root):
     col.grid(row=1, column=0)
     obj = tk.Entry(root, width=20)
     obj.grid(row=2, column=0)
-    path_entry = tk.Entry(root, width=20)
-    path_entry.grid(row=0, column=1)
     button = tk.Button(root, text="Set", width=15, command=set).grid(row=3, column=0)
-    save_button = tk.Button(root, text="Save .map file", command=save).grid(row=2, column=1)
-    open_button = tk.Button(root, text='Load .map file', command=load).grid(row=1, column=1)
+    save_button = tk.Button(root, text="Save .map collision file", command=lambda: save(f"{project_path}\\maps\\collision.map")).grid(row=0, column=2)
+    open_button = tk.Button(root, text='Load .map collision file', command=lambda:load(f"{project_path}\\maps\\collision.map")).grid(row=0, column=1)
     back_button = tk.Button(root, text='Back', command=lambda: back(root)).grid(row=3, column=1)
     root.mainloop()
 
 if __name__ == "__main__":
-    main()
+    start()
